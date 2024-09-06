ed4dfcb2fc822e68a8d110372fc6599c8f3819c5
Byron Streit <158120676+byronscodes@users.noreply.github.com>
2024-09-06 16:44:39 -0400

    parking lot comments

--- ParkingLot.java
+++ ParkingLot.java
@@ -1,12 +1,22 @@
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Class for parking lots
+ * 
+ * @author Sy Traore and Byron Streit
+ * @version 2024.09.06
+ */
 public class ParkingLot {
 
     private String parkingLotId;
     private List<List<Slot>> slots;
 
     /**
+     * Creates "slots" array list and finds number of slots per floor for specific parking lot
+     * Then adds vehicle-specific slots up until the number of slots per floor
+     * Then does the same for all floors within the parking lot
+     * 
      * @param parkingLotId
      *      ID of the entire parking lot
      * @param nfloors
